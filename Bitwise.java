e6ef6d3fee7a7e7d0390da88b04217df38555be9
Shiva-kumar20 <shivakumar.630485@gmail.com>
2025-01-24 00:08:29 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,10 @@
+public class Bitwise {
+    static int a=10;
+    static int b=20;
+   public static void main(String[] args){ 
+int result=a<<2;
+int answer =b>>2;
+System.out.println(result);
+System.out.println(answer);
+   } 
+}
